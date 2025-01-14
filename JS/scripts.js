350f0e93308f4664792601198aad8632b1daa521
Eduardo Soares <eduardosoarespt24@gmail.com>
2025-01-14 20:56:06 -0100

    Change one heigth number
    Forgot to do one from the previous change

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = [{ name: `abra`, type: `psychic`, height: `0,9 m` },
+let pokemonList = [{ name: `abra`, type: `psychic`, height: 0.9} ,
     { name: `hitmonchan`, type: `fighting`, height: 1.4  },
     { name: `kakuna`, type: [`bug`, `poison`], height: 0.6  },
     { name: `magCargo`, type: [`fire`, `Rock`], height: 0.8  }];
