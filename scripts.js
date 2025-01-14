1578e89c1f917ac9d9d5c428ef4998848ac6bfae
Eduardo Soares <eduardosoarespt24@gmail.com>
2025-01-14 11:48:50 -0100

    PokemonList - list of pokemons
    In the simple-js-app project I added an Array with the variable of pokemonList, this Array contains a list of 4 pokemons with their respective names, types and heights, done in JS, this is for the the project of this exercise

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = "Ramen";
-
-document.write(favoriteFood);
+let pokemonList = [{ name: `abra`, type: `psychic`, height: `0,9 m` },
+    { name: `hitmonchan`, type: `fighting`, height: `1.4 m` },
+    { name: `kakuna`, type: [`bug`, `poison`], height: `0.6 m` },
+    { name: `magCargo`, type: [`fire`, `Rock`], height: `0.8 m` }];
+    
